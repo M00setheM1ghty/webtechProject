adbf8270629fd1179b8e5659abf5787ae70e7be7
M00setheM1ghty <129610686+M00setheM1ghty@users.noreply.github.com>
2023-10-27 15:49:50 +0200

    added db connection

--- index.php
+++ index.php
@@ -12,7 +12,19 @@
             <h1>Hotel Hämmerle</h1>
             <p>Willkommen bei uns im schönen Lech am Arlberg!</p>
         </div>
-        
+        <?php
+        require_once('dbaccess.php');
+        $db_obj = new mysqli($servername, $username, $password, $database);
+
+        if ($db_obj->connect_error) {
+            echo "Connection Error: " . $db_obj->connect_error;
+            exit();
+        }
+        $sql =
+            "SELECT * FROM test";
+        $result = $db_obj->query($sql);
+        echo "<pre>" . print_r($result->fetch_array(), true) . "</pre>";
+        ; ?>
     </main>
 </body>
 
